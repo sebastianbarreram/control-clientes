2fa54f650556076b00602eedddf70837d9946cd9
sebastianbarreram <sbarrera.96@hotmail.com>
2022-09-12 14:41:16 -0500

    Routes and dependencies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BoardComponent } from './components/board/board.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { RegisterComponent } from './components/register/register.component';
+import { UpdateClientComponent } from './components/update-client/update-client.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BoardComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'configuration', component: BoardComponent },
+  { path: 'client/update/:id', component: UpdateClientComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BoardComponent } from './components/board/board.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { UpdateClientComponent } from './components/update-client/update-client.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ConfigurationComponent } from './components/configuration/configuration.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BoardComponent,
+    ClientsComponent,
+    UpdateClientComponent,
+    LoginComponent,
+    RegisterComponent,
+    ConfigurationComponent,
+    NotFoundComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
